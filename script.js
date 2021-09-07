c9f1513ebf63c78e41d2a54876a282cc8e3fd4bd
James <ball.j.work@gmail.com>
2021-09-07 14:20:14 +0100

    exercise completed

---
+++ script.js
@@ -0,0 +1,76 @@
+// School class - base
+class School {
+    constructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    } 
+    get name() {
+        return this.name;
+    }
+
+    get level() {
+        return this.level;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+
+    set numberOfStudents(numberOfStudents) {
+        if (typeof numberOfStudents === 'number') this._numberOfStudents = numberOfStudents;
+        else console.log('Invalid input: numberOfStudents must be set to a Number.');
+    }
+
+    quickFacts() {
+        return console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
+    }
+
+    static pickSubstituteTeacher(substituteTeachers) {
+    
+        let randomNumber = Math.floor((Math.random() * substituteTeachers.length));
+        return substituteTeachers[randomNumber];
+    }
+
+}
+
+// primary school class - extends school
+ class PrimarySchool extends School{
+     constructor(name, numberOfStudents, pickUpPolicy){
+     super(name, 'primary', numberOfStudents)
+     this._pickupPolicy = pickUpPolicy;
+     }
+
+     get pickUpPolicy() {
+         return this._pickupPolicy;
+     }
+
+ }
+
+// secondary school class - extends school
+
+class SecondarySchool extends School {
+    constructor(name, numberOfStudents, sportsTeam) {
+        super(name, 'secondary', numberOfStudents)
+        this._sportsTeam = sportsTeam; 
+    }
+
+    get sportsTeam() {
+        console.log(this._sportsTeam);
+    }
+}
+
+
+// create primary school
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+console.log(lorraineHansbury.quickFacts());
+console.log(School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']));
+
+// create secondary school 
+const magnaCartaSchool = new SecondarySchool('The Magna Carta School of Excellence', 1500, ['Rugby', 'Hockey', 'Football', 'Tennis']);
+console.log(magnaCartaSchool);
+console.log(magnaCartaSchool.sportsTeam)
+
+// checking prototypes for fun
+console.dir(Object.getPrototypeOf(magnaCartaSchool));
+console.dir(Object.getPrototypeOf(School))
